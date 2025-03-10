86b2c7165c9fea858d07700576e40b8ae7c2bd2d
youngpuffy <levimuturi2@gmail.com>
2025-03-10 12:10:57 +0300

    complete strings

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser ="levi"
+ 
+let welcomeMessage =`Welcome to Flatbook, ${currentUser}!`
+ 
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+ console.log(shortGreeting);
+ 
